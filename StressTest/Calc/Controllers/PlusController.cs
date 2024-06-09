6aaf83c7851905bdf68358ba683a2f2569fbdc4a
dmitriidemidov <dmitri-demidiv@yandex.ru>
2024-06-09 21:08:52 +0300

    PlusController is added

---
+++ StressTest/Calc/Controllers/PlusController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calc.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PlusController : ControllerBase
+{
+	public PlusController()
+	{
+	}
+}
